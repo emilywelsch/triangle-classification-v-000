808ed79d3e3a6a093c47d4071436c36e92003315
Emily Welsch <emwelsch@gmail.com>
2018-06-03 00:19:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,16 @@
 class Triangle
-  attr_accessor :length1, :length2, :length3
-
+  
   def initialize(length1, length2, length3)
-    @length1 = length1
-    @length2 = length2
-    @length3 = length3
+    @triangle_lengths = []
+    @triangle_lengths << length1
+    @triangle_lengths << length2
+    @triangle_lengths << length3
+  end
 
+  def kind
+    if length1 == length2 == length3
+      return :equilateral
+      
   end
 
 
